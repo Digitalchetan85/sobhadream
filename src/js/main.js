bb2d0154e92caf4fe553a10213b8e087d1425a37
Chetan Kumar N <chetankumar.nv@gmail.com>
2022-05-11 11:59:52 +0530

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,81 @@
+// jQuery(document).ready(function(){
+//     jQuery('.nav-item').on('click', '.nav-item', function(){
+//       jQuery('.nav-item.active').removeClass("active");
+//       jQuery(this).addClass("active");
+//   });
+//   });
+
+
+//   jQuery(document).ready(function () {
+//     (function (jQuery) {
+//         jQuery('#parent').owlCarousel({
+//             loop:true,
+//             margin:10,
+//             nav: true,
+//             navText: ["<i class='fa fa-chevron-circle-left'></i>","<i class='fa fa-chevron-circle-right'></i>"],
+//             responsive:{
+//             0:{
+//                 items:2
+//             },
+//             600:{
+//                 items:2
+//             },
+//             1000:{
+//                 items:4
+//             }
+//             }
+//         });
+//     })(jQuery);
+// });
+
+// jQuery(document).ready(function () {
+//   (function (jQuery) {
+//       jQuery('#testi').owlCarousel({
+//           loop:true,
+//           margin:10,
+//           nav: true,
+//           autoplay: true,
+//           autoplayTimeout: 2000,
+//           navText: ["<i class='fa fa-chevron-circle-left'></i>","<i class='fa fa-chevron-circle-right'></i>"],
+//           responsive:{
+//           0:{
+//               items:1
+//           },
+//           600:{
+//               items:1
+//           },
+//           1000:{
+//               items:1
+//           }
+//           }
+//       });
+//   })(jQuery);
+// });
+
+// jQuery(document).ready(function () {
+//   (function (jQuery) {
+//       jQuery('#clients').owlCarousel({
+//           loop:true,
+//           margin:10,
+//           nav: false,
+//           autoplay: true,
+//           autoplayTimeout: 2000,
+//           navText: ["<i class='fa fa-chevron-circle-left'></i>","<i class='fa fa-chevron-circle-right'></i>"],
+//           responsive:{
+//           0:{
+//               items:1
+//           },
+//           600:{
+//               items:3
+//           },
+//           1000:{
+//               items:6
+//           }
+//           }
+//       });
+//   })(jQuery);
+// });
+
+// jQuery('select').selectpicker();
+
+
